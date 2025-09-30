10c95c21f63e1c4a111bf4e7e0bf7bafa074ac0f
angus <N00241978@iadt.ie>
2025-09-30 17:03:38 +0100

    seeding and some other stuff

---
+++ 2025-Bank-App/database/seeders/CustomerSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        $customers = [];
+
+        for ($i = 0; $i < 10; $i++) {
+            $customers[] = [
+                'name' => $faker->name(),
+                'email' => $faker->unique()->safeEmail(),
+                'phone' => $faker->phoneNumber(),
+                'address' => $faker->address(),
+            ];
+        }
+
+        DB::table('customers')->insert($customers);
+    }
+}
